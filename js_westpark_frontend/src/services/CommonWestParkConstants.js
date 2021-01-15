99ee0854b750fb3dcfe4843802526e2dc390a903
Геннадий <miropolskij@gmail.com>
2021-01-15 13:54:38 +0100

    First commit

--- js_westpark_frontend/src/services/CommonWestParkConstants.js
+++ js_westpark_frontend/src/services/CommonWestParkConstants.js
@@ -1,9 +1,6 @@
  
 //path to cross origin (backend)
 const pathToCrossOrigin = 'http://94.130.181.51:8095/apiWestpark/';
-//'http://94.130.181.51:8094/apiWestpark/';
-//'http://94.130.181.51:8095/apiWestpark/'
-//'http://localhost:8080/apiWestpark/';
 
  
 class CommonWestParkConstants {
